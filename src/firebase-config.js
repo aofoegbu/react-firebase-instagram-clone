4d9005035839c92e5ac3b4f2823ff7ee5689aa61
Ogelo10 <augustineofoegbu1@gmail.com>
2022-01-17 07:07:36 +0100

    React Instagram clone

---
+++ src/firebase-config.js
@@ -0,0 +1,17 @@
+import {initializeApp} from 'firebase/app';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB5d7ESxHzHUFOu-z0wT54ac6fmmax0O_0",
+    authDomain: "react-instagram-clone-cd289.firebaseapp.com",
+    projectId: "react-instagram-clone-cd289",
+    storageBucket: "react-instagram-clone-cd289.appspot.com",
+    messagingSenderId: "34279062381",
+    appId: "1:34279062381:web:405d326798d1e04bfdf864",
+    measurementId: "G-F8MYR5MEW9"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+
+
